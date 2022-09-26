460b70418598a3f6564b1406c25859ef1985aa3b
Eslam <eslam121295@gmail.com>
2022-09-26 02:53:23 +0200

    add all fils

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { HeaderComponent } from './home/home-page/components/header/header.component';
+import { HeroSectionComponent } from './home/home-page/components/hero-section/hero-section.component';
+import { ProjectsCardComponent } from './home/home-page/components/projects-card/projects-card.component';
+import { AboutComponent } from './home/home-page/components/about/about.component';
+import { FooterComponent } from './home/home-page/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    HeroSectionComponent,
+    ProjectsCardComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home-page/components/projects-card/projects-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-card',
+  templateUrl: './projects-card.component.html',
+  styleUrls: ['./projects-card.component.css']
+})
+export class ProjectsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
