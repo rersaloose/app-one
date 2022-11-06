c806a4d74ee5665fa4ae703ae37ba535cb495993
Eslam <eslam121295@gmail.com>
2022-11-06 22:30:26 +0200

    upchanges

--- src/app/home/home-page/components/slid-section/slid-section.component.ts
+++ src/app/home/home-page/components/slid-section/slid-section.component.ts
@@ -10,9 +10,19 @@ import { Product } from '../Model/Products';
 export class SlidSectionComponent implements OnInit {
   imgCollection:Array<Product> = []
   constructor(private slid:SlidService) { }
-
+  imageObject:Array<object>=[]
   ngOnInit(): void {
     this.imgCollection=this.slid.products
+
+    for (let index = 0; index < this.imgCollection.length; index++) {
+      const element = this.imgCollection[index];
+      this.imageObject.push({
+        image: element.src,
+        thumbImage: element.src,
+        alt: element.subtittle,
+        title: element.tittle
+      })
+    }
   }
 
 }
--- src/app/services/slid.service.ts
+++ src/app/services/slid.service.ts
@@ -17,6 +17,12 @@ export class SlidService {
     new Product("assets/team-03.jpg"," Team-3","Ana-Three"),
     new Product("assets/team-04.jpg"," Team-4","Ana-Four"),
     new Product("assets/team-05.png"," Team-5","Ana-Five"),
+    new Product("assets/team-06.png"," Team-6","Ana-six"),
+    new Product("assets/team-01.jpg"," Team-1","Ana-One"),
+    new Product("assets/team-02.jpg"," Team-2","Ana-Two"),
+    new Product("assets/team-03.jpg"," Team-3","Ana-Three"),
+    new Product("assets/team-04.jpg"," Team-4","Ana-Four"),
+    new Product("assets/team-05.png"," Team-5","Ana-Five"),
     new Product("assets/team-06.png"," Team-6","Ana-six")
       ];
 
