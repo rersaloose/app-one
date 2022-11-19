2c2610443ba2ff7b17a0026ce3d7635c84beac88
Your Name <you@example.com>
2022-11-19 06:06:32 -0800

    artcles

--- src/app/home/home-page/components/Articles/Articles.component.ts
+++ src/app/home/home-page/components/Articles/Articles.component.ts
@@ -1,6 +1,6 @@
 import { BlogService } from './../../../../services/blog.service';
 import { Component, OnInit } from '@angular/core';
-import { TitleStrategy } from '@angular/router';
+
 
 @Component({
   selector: 'app-Articles',
@@ -10,10 +10,10 @@ import { TitleStrategy } from '@angular/router';
 export class ArticlesComponent implements OnInit {
 blogObject=[]
   constructor(private BlogService:BlogService) { }
-
   ngOnInit(): void {
   this.BlogService.getBlog().subscribe((e:any)=>{
     this.blogObject=e
+    console.log(e)
   })
   }
 
