600f666f555bcca8d40f758bb98e6ef4a03d8b52
Your Name <you@example.com>
2022-12-19 14:47:51 +0200

    add bnutton group

--- src/app/Admin/controlPanal/Components/product-details/product-details.component.ts
+++ src/app/Admin/controlPanal/Components/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Product } from 'src/app/home/home-page/components/Model/Products';
 import { ProService } from 'src/app/services/product.service';
 
@@ -11,15 +12,45 @@ import { ProService } from 'src/app/services/product.service';
 export class ProductDetailsComponent implements OnInit {
 currntPid:number=0;
 productId:Product= new Product()
-  constructor(private ActivatedRouter:ActivatedRoute ,  private product:ProService) { }
+// prdIds:number[]=[]
+  constructor(private ActivatedRouter:ActivatedRoute ,  private product:ProService , private location:Location , private router:Router) { }
 
   ngOnInit(): void {
-     this.currntPid = Number (this.ActivatedRouter.snapshot.paramMap.get('pid'))
-     console.log(this.currntPid)
-     this.product.getProductByid(this.currntPid).subscribe(e=>{
+    //  this.currntPid = Number (this.ActivatedRouter.snapshot.paramMap.get('pid'))
+    this.ActivatedRouter.paramMap.subscribe((e)=>{
       console.log(e)
-      this.productId=e
+      this.currntPid = Number (e.get('pid'));
+    }) 
+
+     this.product.getProductByid(this.currntPid).subscribe((e)=>{
+      
+      this.productId=e;
      })
+    //  this.product.getProduct().subscribe(res=>{
+    //   this.prdIds=res
+    //  })
+
   }
 
+  Prev(){
+//  var crrindex = this.prdIds.findIndex((e)=>e ==this.currntPid);
+//  console.log(crrindex)
+
+if (this.productId.id > 0) {
+  var prevId = this.productId.id-1;
+  console.log(prevId)
+this.router.navigate(["/products/",prevId])
+  
+}
+
+  }
+
+  BACK(){
+this.location.back();
+  }
+  Next(){
+    var nextId = this.productId.id+1;
+    console.log(nextId)
+  this.router.navigate(["/products/",nextId])
+  }
 }
